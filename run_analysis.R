d3d57161134f365539acc228d473069f6059fded
basacul <lucas.anteloblanco@epfl.ch>
2015-08-20 19:50:28 +0200

    run_analysis.R finished and needs further polishment which will be done on saturday. Starting on ReadMe.md and CodeBook.md

--- run_analysis.R
+++ run_analysis.R
@@ -2,113 +2,103 @@
 ## the Samsung Galaxy S smartphone:
 ##   http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
-#try to implement this function for reading the files
-#      files_list <- list.files(directory, full.names = TRUE)
-#      dat <- data.frame()
-#      
-#      for(i in id)
-#      {
-#            dat <- rbind(dat, read.csv(files_list[i]))
-#      }
-      
-
-
-#data for the project
+#data for the project: hardcoded and each file will be downloaded separately after unzipping
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 date_downloaded <- date()
 download.file(url, destfile = "dataset.zip")
 unzip("dataset.zip")
 
-#1. Merges the training and the test sets to create one data set.
-#features explains the columns of x_train and x_test
-#and activity_labels the columns for y_train and y_test
-features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
-names(features) <- c("Variable", "Feature")
-features <- as.character(features[,2])
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
-names(activity_labels) <- c("Label", "Activity")
+## Reads all the files necessary for the assignment
+##
+      #1. Merges the training and the test sets to create one data set.
+      #features represent the column variables of both x_* files
+      #activity_labels is a reference for the y_* files
+      features <- read.table("./UCI HAR Dataset/features.txt", 
+                             stringsAsFactors = FALSE)
+      activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", 
+                                    stringsAsFactors = FALSE)
+      
+      #Read test files without subdirectories as unnecessary
+      subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", 
+                                 stringsAsFactors = FALSE)
+      x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", 
+                           stringsAsFactors = FALSE)
+      
+      #column represents the activity_label from 1 to 6
+      y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", 
+                           stringsAsFactors = FALSE)
+      
+      #cbind plus add column indicating that it is a test subject with 1
+      test <- cbind(subject_test, 
+                    rep(0, nrow(subject_test)), 
+                    x_test, y_test)
+      names(test) <- c("Subject", "Group" , features[,2] , "Activity")
+      
+      #Now the same thing for train!
+      subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", 
+                                  stringsAsFactors = FALSE)
+      
+      #add column Group with 0 equals subject is from train group
+      subject_train <- cbind(subject_train, rep(1, nrow(subject_train)))
+      x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", 
+                            stringsAsFactors = FALSE)
+      
+      #column represents activity_label from 1 to 6
+      y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt", 
+                            stringsAsFactors = FALSE)
+      train <- cbind(subject_train, x_train, y_train)
+      names(train) <- c("Subject", "Group" , features[,2] , "Activity")
 
-#Downloading test files without subdirectories as unnecessary
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", stringsAsFactors = FALSE)
-
-x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", stringsAsFactors = FALSE)
-
-#column represents the activity_label from 1 to 6
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", stringsAsFactors = FALSE)
-
-#cbind plus add column indicating that it is a test subject with 1
-test <- cbind(subject_test, rep(1, nrow(subject_test)), x_test, y_test)
-names(test) <- c("Subject", "Train" , features , "Activity")
-
-#Now the same thing for train! - oh my god
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", stringsAsFactors = FALSE)
-
-#add column Train with 0 equals train subject
-subject_train <- cbind(subject_train, rep(0, nrow(subject_train)))
-
-x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", stringsAsFactors = FALSE)
-
-#column represents activity_label from 1 to 6
-y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt", stringsAsFactors = FALSE)
+      
 
 
-train <- cbind(subject_train, x_train, y_train)
-names(train) <- c("Subject", "Train" , features , "Activity")
-
+library(dplyr)
 #second column needs to be renamed to Group: 0 = train , 1 = test
-dataframe <- rbind(test,train)
+#so that there is a dataframe where the subject's group is assigned
+dataframe <- bind_rows(test,train)
 
 #remove variables in the working environment to free some memory space
 rm("url","subject_test", "x_test", "y_test", "test", "subject_train", "x_train", "y_train", "train")
 
-#2, Extracts only the measurements on the mean and standard deviation for each measurement. 
-good <-     grepl("mean", colnames(dataframe), ignore.case = TRUE) |
-            grepl("std", colnames(dataframe), ignore.case = TRUE)
+#2, Extracts only the measurements on the mean and standard deviation for each measurement
+# as well as subject and activity column
+subset <- dataframe %>% select(Subject, Activity, contains("mean"), contains("std"))
 
-subset <- dataframe[,good]
-subset <- cbind(dataframe[,c(1,2,ncol(dataframe))],subset)
+subset <- data.frame(subset)
+subset[,2] <- as.character(subset[,2])
 
 #3. Uses descriptive activity names to name the activities in the data set
-#done in point 1 by naming the columns with the help of the features.txt for x and activity_lables.txt for y
+#this is done with as simple for loop - brute force coding
 l <- nrow(subset)
-subset[,3] <- as.character(subset[,3])
 for (i in 1:l){
       
-      if( subset[i,3] == "1"){
-            subset[i,3] <- "walking"
-      }else if(subset[i,3] == "2"){
-            subset[i,3] <- "walking_upstairs"
-      }else if(subset[i,3] == "3"){
-            subset[i,3] <- "walking_downstairs"
-      }else if(subset[i,3] == "4"){
-            subset[i,3] <- "sitting"
-      }else if(subset[i,3] == "5"){
-            subset[i,3] <- "standing"
-      }else if(subset[i,3] == "6"){
-            subset[i,3] <- "laying"
+      if( subset[i,2] == "1"){
+            subset[i,2] <- "walking"
+      }else if(subset[i,2] == "2"){
+            subset[i,2] <- "walking_upstairs"
+      }else if(subset[i,2] == "3"){
+            subset[i,2] <- "walking_downstairs"
+      }else if(subset[i,2] == "4"){
+            subset[i,2] <- "sitting"
+      }else if(subset[i,2] == "5"){
+            subset[i,2] <- "standing"
+      }else if(subset[i,2] == "6"){
+            subset[i,2] <- "laying"
       }else{
             subset[i,3] <- NA
       }
 }
+
 #4. Appropriately labels the data set with descriptive variable names. 
-#?done?  For me it looks ok
+#to do: look in the forum
 
 #5. From the data set in step 4, creates a second, independent tidy data set with 
 #the average of each variable for each activity  and each subject.
-library(dplyr)
-#Remove Train Column
-subset <- select(subset, -Train)
-
-subset_Activity <- select(subset, -Subject)
-subset_Subject <- select(subset, -Activity)
-
-activity <- group_by(subset, Activity)
-subject <- group_by(subset, Subject)
-
-active <- summarize(activity, mean)
-subjective < su
-
+library(plyr)
+tidy_data <- ddply(by_activity_and_subject, .(Subject, Activity), numcolwise(mean))
+tidy_data
 
 #   upload the tidy data set as .txt file created with write.table() using 
 #   row.name = FALSE (do not cut and paste a dataset int the text box, as 
 #   this may cause errors saving your submission)
+write.table(tidy_data, file = "tidy_data.txt", row.names = FALSE)
